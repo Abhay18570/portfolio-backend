3f54a9258bbc8f76a0ea55ec94aedbf0f047d594
Abhay18570 <abhaysonone0@gmail.com>
2026-07-02 11:56:46 +0530

    added home controller

---
+++ portfolio-backend/src/main/java/com/abhay/portfolio_backend/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.abhay.portfolio_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Portfolio backend is running successfully";
+    }
+}
