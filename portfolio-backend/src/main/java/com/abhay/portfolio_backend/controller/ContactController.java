d4fa6adb17d6b0387f45f9f551752d762c826502
Abhay18570 <abhaysonone0@gmail.com>
2026-07-02 12:37:28 +0530

    changes in contactcontroller

--- portfolio-backend/src/main/java/com/abhay/portfolio_backend/controller/ContactController.java
+++ portfolio-backend/src/main/java/com/abhay/portfolio_backend/controller/ContactController.java
@@ -2,33 +2,42 @@ package com.abhay.portfolio_backend.controller;
 
 import com.abhay.portfolio_backend.dto.ContactRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "*")
 public class ContactController {
 
     @Autowired
     private JavaMailSender mailSender;
 
     @PostMapping
-    public String sendContactMessage(@RequestBody ContactRequest request) {
+    public ResponseEntity<String> sendContactMessage(@RequestBody ContactRequest request) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setTo("abhaysonone0@gmail.com");
-        message.setSubject("New Message From Portfolio");
-        message.setText(
-                "Name: " + request.getName() + "\n" +
-                "Email: " + request.getEmail() + "\n\n" +
-                "Message:\n" + request.getMessage()
-        );
+            message.setFrom("abhaysonone0@gmail.com");
+            message.setTo("abhaysonone0@gmail.com");
+            message.setSubject("New Portfolio Message from " + request.getName());
 
-        mailSender.send(message);
+            message.setText(
+                    "Name: " + request.getName() + "\n" +
+                    "Email: " + request.getEmail() + "\n\n" +
+                    "Message:\n" + request.getMessage()
+            );
 
-        return "Message sent successfully";
+            mailSender.send(message);
+
+            return ResponseEntity.ok("Message sent successfully");
+
+        } catch (MailException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Email sending failed: " + e.getMessage());
+        }
     }
 }
